1f7731dcdca0ff7142be11e1b9cfe2e5186409d9
Mi Lainmar Oo <milainmar13@gmail.com>
2023-08-30 14:46:13 +1000

    deploy app to github

---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
+
+const app = express();
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// cors middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
+
+// Create routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the RandomIdeas API' });
+});
+
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
+
+// Create server
+app.listen(port, () => console.log(`Server listening on port ${port}`));
